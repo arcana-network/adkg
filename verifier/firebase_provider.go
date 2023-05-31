a2e85108d4844b59d34dd114d9544b739c88a8fb
mmjee <root@git.maharshi.ninja>
2023-05-31 15:08:43 +0530

    Handle expired keys inside Firebase (#13)

--- verifier/firebase_provider.go
+++ verifier/firebase_provider.go
@@ -28,19 +28,30 @@ type FirebaseVerifierParams struct {
 }
 
 func NewFirebaseProvider() (*FirebaseProvider, error) {
+	f := &FirebaseProvider{
+		Leeway: 120 * time.Second,
+	}
+	err := f.initializeKeys()
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func (f *FirebaseProvider) initializeKeys() error {
 	// https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
 	keyMap := make(map[string]string, 4)
 	parsedKM := make(map[string]crypto.PublicKey, 4)
 
 	rsp, err := req.R().SetSuccessResult(&keyMap).Get(FirebaseKeyListURL)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var expiry *time.Time = nil
 	ctrlHdr := rsp.Header.Get("Cache-Control")
 	if len(ctrlHdr) != 0 {
-		// This not a valid algorithm to parse the token, when the time comes, we should likely see use github.com/pquerna/cachecontrol/cacheobject's ParseResponseCacheControl
+		// This not a valid algorithm to parse the token, when the time comes, we should likely see use github.com/pquerna/cachecontrol/cacheobject's ParseResponseCacheControl.
 		parts := strings.Split(ctrlHdr, ",")
 		for _, part := range parts {
 			sides := strings.Split(strings.TrimSpace(part), "=")
@@ -52,7 +63,7 @@ func NewFirebaseProvider() (*FirebaseProvider, error) {
 			}
 			rhs, err := strconv.ParseUint(sides[1], 10, 32)
 			if err != nil {
-				return nil, err
+				return err
 			}
 			_exp := time.Now().Add(time.Duration(rhs) * time.Second)
 			expiry = &_exp
@@ -63,27 +74,25 @@ func NewFirebaseProvider() (*FirebaseProvider, error) {
 	for k, v := range keyMap {
 		pk, err := jwt.ParseRSAPublicKeyFromPEM([]byte(v))
 		if err != nil {
-			return nil, err
+			return err
 		}
 		parsedKM[k] = pk
 	}
 
-	return &FirebaseProvider{
-		Leeway:           120 * time.Second,
-		signingKeys:      parsedKM,
-		signingKeyExpiry: expiry,
-	}, nil
+	f.signingKeys = parsedKM
+	f.signingKeyExpiry = expiry
+	return nil
 }
 
-func (f FirebaseProvider) ID() string {
+func (f *FirebaseProvider) ID() string {
 	return "firebase"
 }
 
-func (f FirebaseProvider) CleanToken(s string) string {
+func (f *FirebaseProvider) CleanToken(s string) string {
 	return strings.Trim(s, " ")
 }
 
-func (f FirebaseProvider) verifyJWT(token *jwt.Token) (interface{}, error) {
+func (f *FirebaseProvider) verifyJWT(token *jwt.Token) (interface{}, error) {
 	if token.Method.Alg() != "RS256" {
 		return nil, errors.New("invalid signing algorithm")
 	}
@@ -103,7 +112,15 @@ func (f FirebaseProvider) verifyJWT(token *jwt.Token) (interface{}, error) {
 
 }
 
-func (f FirebaseProvider) Verify(message *bijson.RawMessage, params *common.VerifierParams) (verified bool, verifierID string, err error) {
+func (f *FirebaseProvider) Verify(message *bijson.RawMessage, params *common.VerifierParams) (verified bool, verifierID string, err error) {
+	// Reinitialize the keys if they are expired already
+	if f.signingKeyExpiry.Before(time.Now()) {
+		err := f.initializeKeys()
+		if err != nil {
+			return false, "", err
+		}
+	}
+
 	var p FirebaseVerifierParams
 	if err := bijson.Unmarshal(*message, &p); err != nil {
 		return false, "", err
