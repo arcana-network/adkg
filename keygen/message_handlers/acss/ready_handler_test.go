34dfbd51a6fedb36f9a6cf086dfe3aeb1bc9c889
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2024-02-01 13:51:42 -0600

    Test for pointing out c.RC / len(keygen.ReadyStore) difference in `ready_handler`.

--- keygen/message_handlers/acss/ready_handler_test.go
+++ keygen/message_handlers/acss/ready_handler_test.go
@@ -107,8 +107,8 @@ func TestReceivedThresholdReadyMsgs(t *testing.T) {
 /*
 Function: Process
 Case: 
-- a node receives (f+1)-th `Ready` messages
-- has received f+1 echo messages
+- a node receives (f+1)-th `Ready` message - by setting RC to f manually
+- has received f+1 echo messages - by setting c.EC manually
 - hasn't broadcast `Ready` message yet
 Expects: 
 - ReadySent is set to true
@@ -153,6 +153,69 @@ func TestMustSendReady(t *testing.T) {
 	// 3. no `Output` message should have been sent
 	// So messageCount should be 2 because of initial msg and broadcasted Ready msg
 	assert.True(t, node0.messageCount == 2)
+}
+
+/*
+NOTE
+Replacing the check to broadcast a Ready message:
+	if c.RC >= f+1 && !c.ReadySent && c.EC >= f+1 {
+with this line will make the test run:
+	if len(keygen.ReadyStore) >= f+1 && !c.ReadySent && c.EC >= f+1 {
+*/
+/*
+Function: Process
+Case: 
+- a node receives (f+1)-th `Ready` message - by sending f+1 Ready msgs
+- has received f+1 echo messages - by setting c.EC manually
+- hasn't broadcast `Ready` message yet
+Expects: 
+- ReadySent is set to true
+- `Ready` message gets broadcasted
+*/
+func TestMustSendReady2(t *testing.T) {
+	// (Node 3 is the dealer of this round and is the one that sends shares to all nodes)
+	// Node 0 is the receiver of all the ready msg
+	// Node 1 is the sender of the (f+1)-th message
+	nodes, transport, node0, round, hash, encodedShares := setupEchoHandlerTest()
+
+	defaultKeygen := NewDefaultKeygen(round)
+	keygen, _ := node0.State().KeygenStore.GetOrSetIfNotComplete(round.ID(), defaultKeygen)
+	
+	curve := common.CurveName(c.Name)
+	senderReady1 := nodes[1]
+	msgToSend1, _ := NewReadyMessage(round.ID(), encodedShares[senderReady1.id-1], hash, curve)
+	msg1 := EchoMessage{
+		round.ID(),
+		EchoMessageType,
+		common.CurveName(c.Name),
+		encodedShares[senderReady1.id-1],
+		hash,
+	}
+	cid := msg1.Fingerprint()
+	c := common.GetCStore(keygen, cid)
+	// Set Echo count for this message to f+1
+	c.EC = f+1
+
+	// prep another f Ready messages
+	senderReady2 := nodes[2]
+	msgToSend2, _ := NewReadyMessage(round.ID(), encodedShares[senderReady2.id-1], hash, curve)
+	senderReady3 := nodes[3]
+	msgToSend3, _ := NewReadyMessage(round.ID(), encodedShares[senderReady3.id-1], hash, curve)
+
+	// Node0 receives f Ready messages
+	node0.ReceiveMessage(senderReady2.Details(), *msgToSend2)
+	node0.ReceiveMessage(senderReady3.Details(), *msgToSend3)
+
+	// Then, Ready msg f+1 is sent by the message for which Echo count is f+1
+	node0.ReceiveMessage(senderReady1.Details(), *msgToSend1)
+
+	time.Sleep(1 * time.Second)
+	
+	// Checks
+	// 1. node0 should have broadcast a Ready message
+	assert.Equal(t, 1, countBroadcastedReadyMessages(transport))
+	// 2. ReadySent must now be set to true
+	assert.True(t, c.ReadySent)
 }
 
 func countBroadcastedReadyMessages(transport *MockTransport) int {
