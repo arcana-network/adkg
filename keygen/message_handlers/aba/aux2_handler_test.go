b46153ab29df27f347857ef2ea3dc5940174d3ea
Manish Kumar <kumarmanish19963@gmail.com>
2024-02-12 23:15:57 +0530

    aux2 handler test

--- keygen/message_handlers/aba/aux2_handler_test.go
+++ keygen/message_handlers/aba/aux2_handler_test.go
@@ -16,7 +16,7 @@ Function: Process
 Case: f+1 NewAux2Message messages for this round, r and v have been received and node[n-1] hasn't broadcast NewAux2Message yet
 Expects: node[n-1] sends NewCoinInitMessage
 */
-//TODO: incomplete
+
 func TestSendAux2Msg(t *testing.T) {
 	r := 0
 	vote := 1
@@ -81,6 +81,68 @@ func TestSendAux2Msg(t *testing.T) {
 	assert.Equal(t, Contains(store.Values("aux2", r, vote), nodes[0].id), true)
 	sessionStore, complete := nodes[0].State().SessionStore.GetOrSetIfNotComplete(round.ADKGID, common.DefaultADKGSession())
 	assert.Equal(t, sessionStore.ABAComplete, false)
+}
+
+/*
+Function: Process
+Case: receives double Aux2 message (from same sender)
+Expects: no message broadcast even though f+1 Aux2 messages are received
+*/
+func TestAux2AlreadyReceived(t *testing.T) {
+	r := 0
+	vote := 1
+	transport, nodes, msg, round := Aux2TestSetup(r, vote)
+
+	receiverNode := nodes[n-1]
+	// node[n-1] received Aux2 messages from nodes 0 through f-1
+	store, _ := receiverNode.State().ABAStore.GetOrSetIfNotComplete(round.ID(), common.DefaultABAStore())
+	for i := 0; i < f; i++ {
+		store.SetValues("aux2", r, vote, nodes[i].id)
+	}
+
+	// Node0 again sends Aux2 message which triggers early return
+	receiverNode.ReceiveMessage(nodes[0].Details(), *msg)
+	time.Sleep(1 * time.Second)
+
+	// Check whether msg was broadcasted
+	broadcastedMessages := transport.GetBroadcastedMessages()
+	assert.Equal(t, 0, len(broadcastedMessages), "No message should have been broadcasted")
+
+	//The receiveMessage is only called once and inside of aux2_handler it does not call receiveMessage again since the message already sent and encounters an early return
+	assert.Equal(t, receiverNode.messageCount, 1)
+}
+
+/*
+Function: Process
+Case: keygen process already completed
+Expects: no message broadcast even though f+1 Aux2 messages are received
+*/
+func TestKeygenAlreadyCompleteAux2(t *testing.T) {
+	r := 0
+	vote := 1
+	transport, nodes, msg, round := Aux2TestSetup(r, vote)
+
+	receiverNode := nodes[n-1]
+
+	// Set the keygen state to completed
+	state := receiverNode.State()
+	state.ABAStore.Complete(round.ID())
+
+	// Send f+1 Aux2 messages, which normally should trigger sending coinInit message
+	// but since keygen is marked complete won't trigger broadcast
+	for i := 0; i < n; i++ {
+		receiverNode.ReceiveMessage(nodes[i].Details(), *msg)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	// Check that whether msg was broadcasted
+	broadcastedMessages := transport.GetBroadcastedMessages()
+	assert.Equal(t, 0, len(broadcastedMessages), "No message should have been broadcasted")
+
+	//The receiveMessage is only called once and inside of aux2_handler it does not call receiveMessage again since the message already sent and encounters an early return
+	//It is done for each node hence 7 message count
+	assert.Equal(t, receiverNode.messageCount, 7)
 }
 
 func Aux2TestSetup(r, vote int) (*MockTransport, []*Node, *common.DKGMessage, common.RoundDetails) {
