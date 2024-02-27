7a5d7aae4d40763bbd2cf9161009bf29d8ff4675
Manish Kumar <kumarmanish19963@gmail.com>
2024-02-27 20:16:13 +0530

    test for predicate

--- common/sharing/encryption_test.go
+++ common/sharing/encryption_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/coinbase/kryptology/pkg/core/curves"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -77,4 +78,62 @@ func generateRandomPlaintext(length int) ([]byte, error) {
 	}
 
 	return randomBytes, nil
+}
+
+func TestPredicate(t *testing.T) {
+
+	//Checked for both K256 and ED25519
+	curve := curves.K256()
+
+	//generate random keys for all nodes
+	NodesSecretKey := make([]curves.Scalar, 0)
+	NodesPublicKey := make([]curves.Point, 0)
+
+	for i := 0; i < 7; i++ {
+		nodeSecret := curve.Scalar.Random(rand.Reader)
+		NodesSecretKey = append(NodesSecretKey, nodeSecret)
+		NodesPublicKey = append(NodesPublicKey, curve.NewGeneratorPoint().Mul(nodeSecret))
+	}
+
+	//dealer's Ephemeral key
+	DealerSecretKey := curve.Scalar.Random(rand.Reader)
+	DealerPublicKey := curve.NewGeneratorPoint().Mul(DealerSecretKey)
+
+	//secret to be shared
+	secret := curve.Scalar.Random(rand.Reader)
+	commitments, shares, _ := GenerateCommitmentAndShares(secret, 3, 7, curve)
+
+	// Compress commitments
+	compressedCommitments := CompressCommitments(commitments)
+
+	// Init share map
+	shareMap := make(map[uint32][]byte, 7)
+
+	// encrypt each share with node respective generated symmetric key using Ephemeral Private key and add to share map
+	for i, share := range shares {
+
+		if NodesPublicKey[i] == nil {
+			log.Errorf("Couldn't obtain public key for node with id=%v", share.Id)
+			return
+		}
+
+		cipherShare, err := EncryptSymmetricCalculateKey(share.Bytes(), NodesPublicKey[i], DealerSecretKey)
+
+		if err != nil {
+			log.Errorf("Error while encrypting secret share, err=%v", err)
+			return
+		}
+		log.Debugf("CIPHER_SHARE=%v", cipherShare)
+		shareMap[share.Id] = cipherShare
+	}
+
+	//test predicate
+	for i := 0; i < 7; i++ {
+		shares_node2 := shareMap[uint32(i+1)][:]
+		//decrypting with dealer's public Ephemeral Key and node's private key
+		symm_key2, _ := CalculateSharedKey(DealerPublicKey, NodesSecretKey[i])
+		_, _, verified_old := Predicate(symm_key2, shares_node2, compressedCommitments, 3, curves.K256())
+		assert.True(t, verified_old)
+	}
+
 }
