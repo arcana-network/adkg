0144d20acac513e7ce1133eaf4734a2fb49cbb71
Manish Kumar <kumarmanish19963@gmail.com>
2024-02-28 19:36:46 +0530

    documentation & elaborate tests for NIZKP, propose handler modified

--- dpss/message_handlers/dacss/propose_handler.go
+++ dpss/message_handlers/dacss/propose_handler.go
@@ -2,9 +2,14 @@ package dacss
 
 import (
 	"encoding/json"
+	"math/big"
 
 	"github.com/arcana-network/dkgnode/common"
+	"github.com/arcana-network/dkgnode/common/sharing"
+	"github.com/arcana-network/dkgnode/keygen/common/acss"
 	"github.com/arcana-network/dkgnode/keygen/messages"
+	log "github.com/sirupsen/logrus"
+	"github.com/vivint/infectious"
 )
 
 var AcssProposeMessageType common.MessageType = "Acss_propose"
@@ -13,62 +18,18 @@ type AcssProposeMessage struct {
 	RoundID            common.PSSRoundID
 	NewCommittee       bool
 	Kind               common.MessageType
-	Curve              common.CurveName
+	CurveName          common.CurveName
 	Data               messages.MessageData
 	EphemeralPublicKey []byte // the dealer's ephemeral publicKey
 
 }
-
-// convert json to acssProposeMessage
-// func (m *AcssProposeMessage) UnmarshalJSON(data []byte) error {
-// 	type Alias AcssProposeMessage
-// 	aux := &struct {
-// 		EphemeralPublicKey json.RawMessage `json:"EphemeralPublicKey"`
-// 		*Alias
-// 	}{
-// 		Alias: (*Alias)(m),
-// 	}
-// 	if err := json.Unmarshal(data, aux); err != nil {
-// 		return err
-// 	}
-
-// 	if len(aux.EphemeralPublicKey) > 0 {
-// 		epk, err := common.PointUnmarshalJson([]byte(aux.EphemeralPublicKey))
-// 		if err != nil {
-// 			return err
-// 		}
-// 		m.EphemeralPublicKey = epk
-// 	}
-
-// 	return nil
-// }
-
-// convert acssProposeMessage to json
-// func (m *AcssProposeMessage) MarshalJSON() ([]byte, error) {
-// 	// Convert EphemeralPublicKey to a suitable JSON representation
-// 	epkJSON, err := common.PointMarshalJson(m.EphemeralPublicKey)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Marshal the rest of AcssProposeMessage as usual, but replace
-// 	// EphemeralPublicKey with its JSON representation
-// 	type Alias AcssProposeMessage // Prevent recursion
-// 	return json.Marshal(&struct {
-// 		EphemeralPublicKey json.RawMessage `json:"EphemeralPublicKey"`
-// 		*Alias
-// 	}{
-// 		EphemeralPublicKey: json.RawMessage(epkJSON),
-// 		Alias:              (*Alias)(m),
-// 	})
-// }
 
 func NewAcssProposeMessageroundID(roundID common.PSSRoundID, msgData messages.MessageData, curveName common.CurveName, isNewCommittee bool, ephemeralPublicKey []byte) (*common.PSSMessage, error) {
 	m := AcssProposeMessage{
 		RoundID:            roundID,
 		NewCommittee:       isNewCommittee,
 		Kind:               AcssProposeMessageType,
-		Curve:              curveName,
+		CurveName:          curveName,
 		Data:               msgData,
 		EphemeralPublicKey: ephemeralPublicKey,
 	}
@@ -81,94 +42,109 @@ func NewAcssProposeMessageroundID(roundID common.PSSRoundID, msgData messages.Me
 	return &msg, nil
 }
 
-// func (msg *HbAacssProposeMessage) Process(sender common.NodeDetails, self common.PSSParticipant) {
+func (msg *AcssProposeMessage) Process(sender common.NodeDetails, self common.PSSParticipant) {
 
-// 	log.Debugf("Received Propose message from %d on %d", sender.Index, self.Details().Index)
-// 	log.Debugf("Propose: Node=%d, Value=%v", self.Details().Index, msg.Data)
+	log.Debugf("Received Propose message from %d on %d", sender.Index, self.Details().Index)
+	log.Debugf("Propose: Node=%d, Value=%v", self.Details().Index, msg.Data)
 
-// 	leader, err := msg.RoundID.Leader()
-// 	if err != nil {
-// 		log.Debugf("Cound not get leader from roundID, err=%s", err)
-// 		return
-// 	}
-// 	sender_big_int := *new(big.Int).SetInt64(int64(sender.Index))
+	leader, err := msg.RoundID.Leader()
+	if err != nil {
+		log.Debugf("Cound not get leader from roundID, err=%s", err)
+		return
+	}
+	senderId := *new(big.Int).SetInt64(int64(sender.Index))
 
-// 	// If leader of the round is not sender skip
-// 	if leader.Cmp(&sender_big_int) != 0 {
-// 		return
-// 	}
+	// If leader of the round is not sender skip
+	if leader.Cmp(&senderId) != 0 {
+		return
+	}
 
-// 	// Generated shared symmetric key
-// 	n, k, _ := self.Params()
+	// Generated shared symmetric key
+	n, k, _ := self.Params()
 
-// 	dealerKey := self.PublicKey(int(leader.Int64()), msg.NewCommittee)
+	curve := common.CurveFromName(msg.CurveName)
+	dealerKey, err := curve.Point.FromAffineCompressed(msg.EphemeralPublicKey)
+	if err != nil {
+		log.Errorf("AcssProposeMessage: error constructing the EphemeralPublicKey: %v", err)
+		return
+	}
 
-// 	priv := self.PrivateKey()
-// 	key := acss.SharedKey(priv, dealerKey)
+	priv := self.PrivateKey()
+	key, err := sharing.CalculateSharedKey(dealerKey, priv)
+	if err != nil {
+		log.Errorf("AcssProposeMessage: error calculating shared key: %v", err)
+		return
+	}
 
-// 	// Verify self share against commitments.
-// 	log.Debugf("Going to verify predicate for node=%d", self.Details().Index)
-// 	log.Debugf("IMP1: round=%s, node=%d, msg=%v", msg.RoundID, self.Details().Index, msg.Data)
+	// Verify self share against commitments.
+	log.Debugf("Going to verify predicate for node=%d", self.Details().Index)
+	log.Debugf("IMP1: round=%s, node=%d, msg=%v", msg.RoundID, self.Details().Index, msg.Data)
 
-// 	_, _, verified := acss.Predicate(key[:], msg.Data.ShareMap[uint32(self.Details().Index)][:],
-// 		msg.Data.Commitments[:], k, msg.Curve)
+	_, _, verified := sharing.Predicate(key, msg.Data.ShareMap[uint32(self.Details().Index)][:],
+		msg.Data.Commitments[:], k, common.CurveFromName(msg.CurveName))
 
-// 	// If verified, send echo to each node
-// 	if verified {
+	//If verified, means the share is encrypted correctly and the commitments is also verified
+	// If verified, send echo to each node
+	if verified {
 
-// 		// Store dealerPublicKey
-// 		//TODO: GetSessionStoreFromRoundID is not defined
-// 		// TODO: The variable s is not being used.
+		// Store dealerPublicKey
+		//TODO: GetSessionStoreFromRoundID is not defined
+		// TODO: The variable s is not being used.
 
-// 		// s, err := common.GetSessionStoreFromRoundID(msg.RoundID, self)
-// 		// if err != nil {
-// 		// 	log.Debugf("Could not get session store for roundID=%s, self=%d", msg.RoundID, self.ID())
-// 		// 	return
-// 		// }
-// 		// s.Lock()
-// 		// defer s.Unlock()
+		// s, err := common.GetSessionStoreFromRoundID(msg.RoundID, self)
+		// if err != nil {
+		// 	log.Debugf("Could not get session store for roundID=%s, self=%d", msg.RoundID, self.ID())
+		// 	return
+		// }
+		// s.Lock()
+		// defer s.Unlock()
 
-// 		// Starts the RBC protocol.
-// 		// Create Reed-Solomon encoding. This is part of the RBC protocol.
-// 		f, err := infectious.NewFEC(k, n)
-// 		if err != nil {
-// 			log.Debugf("error during creation of fec, err=%s", err)
-// 			return
-// 		}
+		// Starts the RBC protocol.
+		// Create Reed-Solomon encoding. This is part of the RBC protocol.
+		f, err := infectious.NewFEC(k, n)
+		if err != nil {
+			log.Debugf("error during creation of fec, err=%s", err)
+			return
+		}
 
-// 		// Serialize data
-// 		msg_bytes, err := msg.Data.Serialize()
-// 		// msg, err := m.data.Serialize()
-// 		if err != nil {
-// 			log.Debugf("error during data serialization of MsgData, err=%s", err)
-// 			return
-// 		}
+		// Serialize data
+		msg_bytes, err := msg.Data.Serialize()
 
-// 		// This corresponds to Line 8, Algorithm 4 of "Asynchronous data disemination and applications."
-// 		msg_hash := common.HashByte(msg_bytes)
+		if err != nil {
+			log.Debugf("error during data serialization of MsgData, err=%s", err)
+			return
+		}
 
-// 		// Obtain Reed-Solomon shards.
-// 		shares, err := acss.Encode(f, msg_hash)
-// 		if err != nil {
-// 			log.Debugf("error during fec encoding, err=%s", err)
-// 			return
-// 		}
+		// This corresponds to Line 8, Algorithm 4 of "Asynchronous data disemination and applications."
+		msg_hash := common.HashByte(msg_bytes)
 
-// 		for _, n := range self.Nodes(msg.NewCommittee) {
-// 			log.Debugf("Sending echo: from=%d, to=%d", self.Details().Index, n.Index)
-// 			go func(node common.NodeDetails) {
+		// Obtain Reed-Solomon shards.
+		shares, err := acss.Encode(f, msg_hash)
+		if err != nil {
+			log.Debugf("error during fec encoding, err=%s", err)
+			return
+		}
 
-// 				//This instruction corresponds to Line 10, Algorithm 4 from
-// 				//"Asynchronous data disemination and applications."
-// 				echoMsg, err := NewDacssEchoMessage(msg.RoundID, shares[node.Index-1], msg_hash, msg.Curve, self.Details().Index, msg.NewCommittee)
-// 				if err != nil {
-// 					log.WithField("error", err).Error("NewDacssEchoMessage")
-// 					return
-// 				}
-// 				self.Send(node, *echoMsg)
-// 			}(n)
-// 		}
-// 	} else {
-// 		log.Debugf("Predicate failed on %d for propose message by %d", self.Details().Index, sender.Index)
-// 	}
-// }
+		for _, n := range self.Nodes(msg.NewCommittee) {
+			log.Debugf("Sending echo: from=%d, to=%d", self.Details().Index, n.Index)
+			go func(node common.NodeDetails) {
+
+				//This instruction corresponds to Line 10, Algorithm 4 from
+				//"Asynchronous data disemination and applications."
+				echoMsg, err := NewDacssEchoMessage(msg.RoundID, shares[node.Index-1], msg_hash, common.CurveFromName(msg.CurveName), self.Details().Index, msg.NewCommittee)
+				if err != nil {
+					log.WithField("error", err).Error("NewDacssEchoMessage")
+					return
+				}
+				self.Send(node, *echoMsg)
+			}(n)
+		}
+	} else {
+
+		//If verified is false, that means either an error occured while decrypting share or shares not verified.
+		//In that case send implicate with the ephemeral public key of the dealer
+		//TODO: IMPLICATE
+
+		log.Debugf("Predicate failed on %d for propose message by %d", self.Details().Index, sender.Index)
+	}
+}
