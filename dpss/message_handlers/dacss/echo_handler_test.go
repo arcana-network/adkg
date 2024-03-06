dece1e38050e90f6a0e2369703c19cee2f567a97
hdvanegasm <hdvanegasm@gmail.com>
2024-03-06 11:50:44 -0500

    Added first test for EchoHandler

--- dpss/message_handlers/dacss/echo_handler_test.go
+++ dpss/message_handlers/dacss/echo_handler_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/arcana-network/dkgnode/common"
 	"github.com/arcana-network/dkgnode/common/sharing"
 	"github.com/arcana-network/dkgnode/keygen/common/acss"
+	"github.com/stretchr/testify/assert"
 	"github.com/torusresearch/bijson"
 
 	testutils "github.com/arcana-network/dkgnode/dpss/test_utils"
@@ -28,12 +29,25 @@ accordingly and send a message
 Expectations:
 - the node increments the counter of echo messages.
 */
-// TODO Finish this.
 func TestIncrement(test *testing.T) {
 	// Setup the parties
 	defaultSetup := testutils.DefaultTestSetup()
 	testSender, testRecvr := defaultSetup.GetTwoOldNodesFromTestSetup()
-	transport := testDealer.Transport
+
+	// Set the round parameters
+	id := big.NewInt(1)
+	pssRoundDetails := common.PSSRoundDetails{
+		PssID:  common.NewPssID(*id),
+		Dealer: testSender.Details(),
+	}
+	acssRoundDetails := common.ACSSRoundDetails{
+		PSSRoundDetails: pssRoundDetails,
+		ACSSCount:       1,
+	}
+
+	// Sets up the RBC state for the sender and receiver to be empty.
+	testRecvr.SetupRBCState(acssRoundDetails)
+	testSender.SetupRBCState(acssRoundDetails)
 
 	ephemeralKeypairSender := common.GenerateKeyPair(curves.K256())
 
@@ -41,26 +55,41 @@ func TestIncrement(test *testing.T) {
 		testSender,
 		testRecvr,
 		ephemeralKeypairSender,
+		acssRoundDetails,
 	)
 	if err != nil {
-
+		test.Errorf("Error creating the echo message: %v", err)
 	}
+
+	echoMsg.Process(testSender.Details(), testRecvr)
+
+	testRecvr.State().AcssStore.Lock()
+	acssState, stateExists, err := testRecvr.State().AcssStore.Get(
+		echoMsg.ACSSRoundDetails.ToACSSRoundID(),
+	)
+	if !stateExists {
+		test.Errorf("The state does not exist")
+	}
+	if err != nil {
+		test.Errorf("Error retrieving the state: %v", err)
+	}
+	echoDatabase := acssState.RBCState.ReceivedEcho
+	for id, received := range echoDatabase {
+		if id == testSender.Details().Index {
+			assert.Equal(test, true, received)
+		} else {
+			assert.Equal(test, false, received)
+		}
+	}
+	testRecvr.State().AcssStore.Unlock()
 }
 
 func getTestEchoMsg(
 	sender *testutils.PssTestNode,
 	receiver *testutils.PssTestNode,
 	ephemeralKey common.KeyPair,
+	acssRoundDetails common.ACSSRoundDetails,
 ) (DacssEchoMessage, error) {
-	id := big.NewInt(1)
-	pssRoundDetails := common.PSSRoundDetails{
-		PssID:  common.NewPssID(*id),
-		Dealer: sender.Details(),
-	}
-	acssRoundDetails := common.ACSSRoundDetails{
-		PSSRoundDetails: pssRoundDetails,
-		ACSSCount:       1,
-	}
 
 	n, k, _ := sender.Params()
 
@@ -72,6 +101,7 @@ func getTestEchoMsg(
 		curves.K256(),
 	)
 	if err != nil {
+		log.Errorf("Error creating the secret sharing: %v", err)
 		return DacssEchoMessage{}, err
 	}
 
@@ -83,15 +113,26 @@ func getTestEchoMsg(
 		n,
 		k,
 	)
+	if err != nil {
+		log.Errorf("Error creating the RS shards sharing: %v", err)
+		return DacssEchoMessage{}, err
+	}
 
 	shardReceiver := shards[receiver.Details().Index]
 
 	receiver.State().AcssStore.Lock()
+
 	recvState, stateExists, err := receiver.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
-	if !stateExists || err != nil {
-		return DacssEchoMessage{}, errors.New("Error retrieving the state of the node")
+	if !stateExists {
+		log.Errorf("Error retrieving the sharing - : %v", err)
+		return DacssEchoMessage{}, errors.New("Error retrieving the state of the node. The state does not exists")
+	}
+	if err != nil {
+		log.Errorf("Error retrieving the sharing: %v", err)
+		return DacssEchoMessage{}, err
 	}
 
+	// Store the shards in the internal state of the node.
 	recvState.RBCState.HashMsg = hashMsg
 	recvState.RBCState.OwnReedSolomonShard = shardReceiver
 
