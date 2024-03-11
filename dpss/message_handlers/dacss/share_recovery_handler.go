168ef08366987f73aed142e91cadfea598d15fa2
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2024-03-11 17:52:11 -0600

    Added tests for ShareRecoveryHandler and made small adjustments in ShareRecoveryHandler.

--- dpss/message_handlers/dacss/share_recovery_handler.go
+++ dpss/message_handlers/dacss/share_recovery_handler.go
@@ -3,17 +3,18 @@ package dacss
 import (
 	"github.com/arcana-network/dkgnode/common"
 	"github.com/arcana-network/dkgnode/common/sharing"
+	log "github.com/sirupsen/logrus"
 	"github.com/torusresearch/bijson"
 )
 
+// The message for initializing Share Recovery phase
 var ShareRecoveryMessageType string = "dacss_share_recovery"
 
 type ShareRecoveryMessage struct {
 	ACSSRoundDetails common.ACSSRoundDetails // ID of the specific ACSS round within DPSS.
 	Kind             string                  // Type of the message
 	CurveName        common.CurveName        // Name (indicator) of curve used in the messages.
-	SenderPubkeyHex  string                  // Hex of Compressed Affine Point
-	AcssData         common.AcssData
+	AcssData         common.AcssData         // ShareMap, commitments & dealer's ephemeral pubkey
 }
 
 func NewShareRecoveryMessage(acssRoundDetails common.ACSSRoundDetails, acssData common.AcssData) (*common.PSSMessage, error) {
@@ -32,6 +33,10 @@ func NewShareRecoveryMessage(acssRoundDetails common.ACSSRoundDetails, acssData 
 	return &msg, nil
 }
 
+/*
+If the current node already has *output* a valid share, send ReceiveShareRecoveryMessage to the other nodes.
+Otherwise, we only indicate that we have arrived in the ShareRecovery phase.
+*/
 func (msg *ShareRecoveryMessage) Process(sender common.NodeDetails, self common.PSSParticipant) {
 
 	// Check message comes from node itself
@@ -42,13 +47,14 @@ func (msg *ShareRecoveryMessage) Process(sender common.NodeDetails, self common.
 	self.State().AcssStore.Lock()
 	defer self.State().AcssStore.Unlock()
 
-	dacssState, found, err := self.State().AcssStore.Get(msg.ACSSRoundDetails.ToACSSRoundID())
+	// Retrieve the ACSS state for the specific ACSS round
+	acssState, found, err := self.State().AcssStore.Get(msg.ACSSRoundDetails.ToACSSRoundID())
 	if err != nil || !found {
 		return
 	}
 
 	// If share recovery is already ongoing, return
-	if dacssState.ShareRecoveryOngoing {
+	if acssState.ShareRecoveryOngoing {
 		return
 	}
 
@@ -57,12 +63,17 @@ func (msg *ShareRecoveryMessage) Process(sender common.NodeDetails, self common.
 		state.ShareRecoveryOngoing = true
 	})
 
-	// If the current node already has a valid share,
+	// If the current node already has *output* a valid share,
 	// send ReceiveShareRecoveryMessage to the other nodes
-	if dacssState.SharesValidated {
+
+	// https://eprint.iacr.org/2021/159.pdf 501: "if Pi previously output valid shares (line 307) then
+	// Multicast SKi and return"
+	// A node has a valid share if it has reached the outputHandler in RBC.
+	// At that point ValidShareOutput is set to true in the node's state
+	if acssState.ValidShareOutput && len(acssState.AcssDataHash) != 0 {
 
 		priv := self.PrivateKey()
-		dealerPubKey, err := common.HexToPoint(msg.CurveName, msg.SenderPubkeyHex)
+		dealerPubKey, err := common.HexToPoint(msg.CurveName, msg.AcssData.DealerEphemeralPubKey)
 		if err != nil {
 			return
 		}
@@ -72,10 +83,19 @@ func (msg *ShareRecoveryMessage) Process(sender common.NodeDetails, self common.
 		}
 		curve := common.CurveFromName(msg.CurveName)
 		pubKeyPoint, err := common.PointToCurvePoint(self.Details().PubKey, msg.CurveName)
+		if err != nil {
+			log.Errorf("Error converting pubkey to curve point in Share Recovery for ACSS round %s, err: %s", msg.ACSSRoundDetails.ToACSSRoundID(), err)
+			return
+		}
 		proof := sharing.GenerateNIZKProof(curve, priv, pubKeyPoint, dealerPubKey, symmetricKey, curve.NewGeneratorPoint())
 
 		receiveShareRecoveryMsg, err := NewReceiveShareRecoveryMessage(msg.ACSSRoundDetails, msg.CurveName, symmetricKey.ToAffineCompressed(), proof, msg.AcssData)
+		if err != nil {
+			log.Errorf("Error in creating ReceiveShareRecoveryMessage for ACSS round %s, err: %s", msg.ACSSRoundDetails.ToACSSRoundID(), err)
+			return
+		}
 
+		// Send the information to all other nodes
 		for _, node := range self.Nodes(!self.IsOldNode()) {
 			self.Send(node, *receiveShareRecoveryMsg)
 		}
---
+++ dpss/message_handlers/dacss/share_recovery_handler_test.go
@@ -0,0 +1,282 @@
+package dacss
+
+import (
+	"testing"
+
+	"github.com/arcana-network/dkgnode/common"
+	"github.com/arcana-network/dkgnode/common/sharing"
+	testutils "github.com/arcana-network/dkgnode/dpss/test_utils"
+	"github.com/coinbase/kryptology/pkg/core/curves"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+Happy path: current node has a valid share, so it sends a ReceiveShareRecoveryMessage to the other nodes.
+Also, shareRecoveryOngoing set to true
+
+Other paths:
+- sender not self: TestSenderNotSelfShareRecovery
+- acssState not found: TestNoAcssDataInState
+- acssDataHash length 0: TestAcssDataHashLenZero
+- shareRecoveryOngoing already true: TestShareRecoveryAlreadyTrue
+- ValidShareOutput false: TestValidShareOutputFalse
+*/
+
+/*
+Function: Process
+
+Testcase: "Happy path". The current node has already output a valid share,
+so it sends a ReceiveShareRecoveryMessage to the other nodes.
+
+Expectations:
+1. ReceiveShareRecoveryMessage was sent to all other nodes from committee
+2. shareRecoveryOngoing set to true
+*/
+func TestSendReceiveShareRecoveryMsg(t *testing.T) {
+	node1, _, acssRoundDetails, shareRecoveryMessage := shareRecoveryHappyPathSetup()
+
+	shareRecoveryMessage.Process(node1.Details(), node1)
+
+	// Check 1: ReceiveShareRecoveryMessage was sent to all other nodes from committee
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 7, len(sentMsgs))
+
+	// Check 2: shareRecoveryOngoing set to true
+	acssState, _, _ := node1.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
+	assert.True(t, acssState.ShareRecoveryOngoing)
+
+	// TODO should we check what's in the message?
+}
+
+/*
+Function: Process
+
+Testcase: Sender not self
+
+Expectations:
+1. No message was sent
+2. shareRecoveryOngoing not set to true
+*/
+func TestSenderNotSelfShareRecovery(t *testing.T) {
+	node1, node2, acssRoundDetails, shareRecoveryMessage := shareRecoveryHappyPathSetup()
+
+	shareRecoveryMessage.Process(node2.Details(), node1)
+
+	// Check 1: No message was sent
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 0, len(sentMsgs))
+
+	// Check 2: shareRecoveryOngoing not set to true
+	acssState, _, _ := node1.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
+	assert.False(t, acssState.ShareRecoveryOngoing)
+}
+
+/*
+Function: Process
+
+Testcase: Node has no acssState
+
+Expectations:
+1. No message was sent
+2. acssState still not found afterwards
+*/
+func TestNoAcssDataInState(t *testing.T) {
+	defaultSetup := testutils.DefaultTestSetup()
+	dealer, node1, _ := defaultSetup.GetThreeOldNodesFromTestSetup()
+
+	ephemeralKeypairDealer := common.GenerateKeyPair(curves.K256())
+	n, k, _ := dealer.Params()
+
+	secret := sharing.GenerateSecret(curves.K256())
+	commitment, shares, err := sharing.GenerateCommitmentAndShares(
+		secret,
+		uint32(k),
+		uint32(n),
+		curves.K256(),
+	)
+	if err != nil {
+		log.Errorf("Error generating commitments & shares: %v", err)
+	}
+	compressedCommitments := sharing.CompressCommitments(commitment)
+	shareMap := make(map[string][]byte, n)
+	for _, share := range shares {
+		nodePublicKey := dealer.GetPublicKeyFor(int(share.Id), dealer.IsOldNode())
+
+		cipherShare, err := sharing.EncryptSymmetricCalculateKey(
+			share.Bytes(),
+			nodePublicKey,
+			ephemeralKeypairDealer.PrivateKey,
+		)
+
+		if err != nil {
+			log.Errorf("Error while encrypting secret share, err=%v", err)
+		}
+		log.Debugf("CIPHER_SHARE=%v", cipherShare)
+		pubkeyHex := common.PointToHex(nodePublicKey)
+		shareMap[pubkeyHex] = cipherShare
+	}
+	msgData := common.AcssData{
+		Commitments:           compressedCommitments,
+		ShareMap:              shareMap,
+		DealerEphemeralPubKey: common.PointToHex(ephemeralKeypairDealer.PublicKey),
+	}
+
+	acssRoundDetails := getTestACSSRoundDetails(dealer)
+
+	if err != nil {
+		log.Errorf("Error updating AcssData in state: %v", err)
+	}
+
+	shareRecoveryMessage := ShareRecoveryMessage{
+		ACSSRoundDetails: acssRoundDetails,
+		Kind:             ShareMessageType,
+		CurveName:        common.SECP256K1, // TODO make this flexible and also test ed25119
+		AcssData:         msgData,
+	}
+
+	shareRecoveryMessage.Process(node1.Details(), node1)
+
+	// Check 1: No message was sent
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 0, len(sentMsgs))
+
+	// Check 2: acssState still not existing
+	_, found, _ := node1.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
+	assert.False(t, found)
+}
+
+/*
+Function: Process
+
+Testcase: acssDataHash has length zero
+
+Expectations:
+1. shareRecoveryOngoing is set to true
+2. No message was sent
+*/
+func TestAcssDataHashLenZero(t *testing.T) {
+	node1, _, acssRoundDetails, shareRecoveryMessage := shareRecoveryHappyPathSetup()
+
+	node1.State().AcssStore.UpdateAccsState(acssRoundDetails.ToACSSRoundID(), func(state *common.AccsState) {
+		state.AcssDataHash = []byte{}
+	})
+	shareRecoveryMessage.Process(node1.Details(), node1)
+
+	// Check 1: shareRecoveryOngoing is set to true
+	acssState, _, _ := node1.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
+	assert.True(t, acssState.ShareRecoveryOngoing)
+
+	// Check 2: No message was sent
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 0, len(sentMsgs))
+}
+
+/*
+Function: Process
+
+Testcase: shareRecoveryOngoing already set to true
+
+Expectations:
+1. No message was sent
+*/
+func TestShareRecoveryAlreadyTrue(t *testing.T) {
+	node1, _, acssRoundDetails, shareRecoveryMessage := shareRecoveryHappyPathSetup()
+
+	node1.State().AcssStore.UpdateAccsState(acssRoundDetails.ToACSSRoundID(), func(state *common.AccsState) {
+		state.ShareRecoveryOngoing = true
+	})
+	shareRecoveryMessage.Process(node1.Details(), node1)
+
+	// Check No message was sent
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 0, len(sentMsgs))
+}
+
+/*
+Function: Process
+
+Testcase: current Node doesn't have a valid share from finished RBC
+
+Expectations:
+1. shareRecoveryOngoing is set to true
+2. No message was sent
+*/
+func TestValidShareOutputFalse(t *testing.T) {
+	node1, _, acssRoundDetails, shareRecoveryMessage := shareRecoveryHappyPathSetup()
+
+	node1.State().AcssStore.UpdateAccsState(acssRoundDetails.ToACSSRoundID(), func(state *common.AccsState) {
+		state.ValidShareOutput = false
+	})
+	shareRecoveryMessage.Process(node1.Details(), node1)
+
+	// Check 1: shareRecoveryOngoing is set to true
+	acssState, _, _ := node1.State().AcssStore.Get(acssRoundDetails.ToACSSRoundID())
+	assert.True(t, acssState.ShareRecoveryOngoing)
+
+	// Check 2: No message was sent
+	sentMsgs := node1.Transport.GetSentMessages()
+	assert.Equal(t, 0, len(sentMsgs))
+}
+
+func shareRecoveryHappyPathSetup() (*testutils.PssTestNode, *testutils.PssTestNode, common.ACSSRoundDetails, ShareRecoveryMessage) {
+	defaultSetup := testutils.DefaultTestSetup()
+	dealer, node1, node2 := defaultSetup.GetThreeOldNodesFromTestSetup()
+
+	ephemeralKeypairDealer := common.GenerateKeyPair(curves.K256())
+	n, k, _ := dealer.Params()
+
+	secret := sharing.GenerateSecret(curves.K256())
+	commitment, shares, err := sharing.GenerateCommitmentAndShares(
+		secret,
+		uint32(k),
+		uint32(n),
+		curves.K256(),
+	)
+	if err != nil {
+		log.Errorf("Error generating commitments & shares: %v", err)
+	}
+	compressedCommitments := sharing.CompressCommitments(commitment)
+	shareMap := make(map[string][]byte, n)
+	for _, share := range shares {
+		nodePublicKey := dealer.GetPublicKeyFor(int(share.Id), dealer.IsOldNode())
+
+		cipherShare, err := sharing.EncryptSymmetricCalculateKey(
+			share.Bytes(),
+			nodePublicKey,
+			ephemeralKeypairDealer.PrivateKey,
+		)
+
+		if err != nil {
+			log.Errorf("Error while encrypting secret share, err=%v", err)
+		}
+		log.Debugf("CIPHER_SHARE=%v", cipherShare)
+		pubkeyHex := common.PointToHex(nodePublicKey)
+		shareMap[pubkeyHex] = cipherShare
+	}
+	msgData := common.AcssData{
+		Commitments:           compressedCommitments,
+		ShareMap:              shareMap,
+		DealerEphemeralPubKey: common.PointToHex(ephemeralKeypairDealer.PublicKey),
+	}
+
+	acssRoundDetails := getTestACSSRoundDetails(dealer)
+
+	// The current node already completed the full RBC process and ended up with a valid share (ValidShareOutput = true)
+	hash, _ := common.HashAcssData(msgData)
+	err = node1.State().AcssStore.UpdateAccsState(acssRoundDetails.ToACSSRoundID(), func(state *common.AccsState) {
+		state.AcssDataHash = hash
+		state.ValidShareOutput = true
+	})
+	if err != nil {
+		log.Errorf("Error updating AcssData in state: %v", err)
+	}
+
+	shareRecoveryMessage := ShareRecoveryMessage{
+		ACSSRoundDetails: acssRoundDetails,
+		Kind:             ShareMessageType,
+		CurveName:        common.SECP256K1, // TODO make this flexible and also test ed25119
+		AcssData:         msgData,
+	}
+	return node1, node2, acssRoundDetails, shareRecoveryMessage
+}
