3086135d38fd5495ac485559b5d2eb66e045da1e
hdvanegasm <hdvanegasm@gmail.com>
2024-03-21 16:38:24 -0500

    Finished tests for COMMITMENT handler.

--- dpss/message_handlers/dacss/commitment_handler_test.go
+++ dpss/message_handlers/dacss/commitment_handler_test.go
@@ -1,20 +1,34 @@
 package dacss
 
 import (
+	"crypto/rand"
 	"encoding/hex"
 	"math/big"
+	mrand "math/rand"
 	"testing"
 
 	"github.com/arcana-network/dkgnode/common"
 	"github.com/arcana-network/dkgnode/common/sharing"
 	testutils "github.com/arcana-network/dkgnode/dpss/test_utils"
 	"github.com/coinbase/kryptology/pkg/core/curves"
+	ksharing "github.com/coinbase/kryptology/pkg/sharing"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+Function: Process
+
+Test case: the receiver node receives t + 1 commitment messages that match with
+the commitment in its internal state.
+
+Expectations:
+  - There is a commitment message whose counter is greater or equal than t + 1
+  - The internal commitment hash and the hash of the commitment with count greater
+    or equal than t + 1 should be equal.
+*/
 func TestCommitmentMsgHappyPath(test *testing.T) {
-	commitmentMsg, receiverNode, senderGroup, err := getCommitmentMessageAndNodesSetup()
+	commitmentMsg, receiverNode, senderGroup, _, err := getCommitmentMessageAndNodesSetup()
 	if err != nil {
 		log.WithFields(
 			log.Fields{
@@ -67,10 +81,27 @@ func TestCommitmentMsgHappyPath(test *testing.T) {
 	} else {
 		assert.Equal(test, t+1, stateReceiver.CommitmentCount[hashCommitment])
 	}
+
+	assert.Equal(
+		test,
+		stateReceiver.OwnCommitmentsHash,
+		hashCommitment,
+	)
 }
 
+/*
+Function: Process
+
+Testcase: repeated send of the same commitment. The receiver node receives t + 1
+messages from the same sender node with the same commitment.
+
+Expectations:
+  - There should not be a message with a counter higher than t + 1.
+  - The message received should not count repetitions. Therefore, the count of the
+    hash of the received message should be 1.
+*/
 func TestCommitmentMsgRepeatedMessages(test *testing.T) {
-	commitmentMsg, receiverNode, senderGroup, err := getCommitmentMessageAndNodesSetup()
+	commitmentMsg, receiverNode, senderGroup, _, err := getCommitmentMessageAndNodesSetup()
 	if err != nil {
 		log.WithFields(
 			log.Fields{
@@ -123,7 +154,69 @@ func TestCommitmentMsgRepeatedMessages(test *testing.T) {
 	assert.Equal(test, 1, countMsg)
 }
 
-func getCommitmentMessageAndNodesSetup() (DacssCommitmentMessage, *testutils.PssTestNode, []*testutils.PssTestNode, error) {
+/*
+Function: Process
+
+Testcase: The user receives t + 1 messages with a commitment that does not match the
+hash of the commitments stored in its internal state. The messages are sent by
+different nodes.
+
+Expectations:
+  - The count of the fake hash should increase to t + 1
+  - The share obtained at the end of the protocol is not valid because the commitments
+    do not match with the commitments stored in the internal state.
+*/
+func TestCommitmentModifiedCommitment(test *testing.T) {
+	commitmentMsg, receiverNode, senderGroup, commitments, err := getCommitmentMessageAndNodesSetup()
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"Error":   err,
+				"Message": "Error while creating the commitment message and the nodes",
+			},
+		).Error("TestHappyPath")
+		test.Error("Error creating the commitment message and nodes.")
+	}
+
+	// Change the commitments on purpose in a random position
+	index := mrand.Intn(len(commitments.Commitments))
+
+	// Replaces the commitment in a random position for another different point
+	// in the curve. Then, change the commitments of the created message by this faked
+	// commitment.
+	newPoint := curves.K256().Point.Random(rand.Reader)
+	for newPoint.Equal(commitments.Commitments[index]) {
+		newPoint = curves.K256().Point.Random(rand.Reader)
+	}
+	commitments.Commitments[index] = newPoint
+	compressedFakeCommitment := sharing.CompressCommitments(commitments)
+	hashFakeCommitments := common.HashByte(compressedFakeCommitment)
+	commitmentMsg.CommitmentsHash = hashFakeCommitments
+
+	// Send the fake message.
+	for _, senderNode := range senderGroup {
+		commitmentMsg.Process(senderNode.Details(), receiverNode)
+	}
+
+	stateReceiver, found, err := receiverNode.State().AcssStore.Get(
+		commitmentMsg.ACSSRoundDetails.ToACSSRoundID(),
+	)
+	assert.Nil(test, err)
+	assert.True(test, found)
+
+	_, _, t := receiverNode.Params()
+	assert.Equal(
+		test,
+		t+1,
+		stateReceiver.CommitmentCount[hex.EncodeToString(hashFakeCommitments)],
+	)
+	assert.False(
+		test,
+		stateReceiver.ValidShareOutput,
+	)
+}
+
+func getCommitmentMessageAndNodesSetup() (DacssCommitmentMessage, *testutils.PssTestNode, []*testutils.PssTestNode, *ksharing.FeldmanVerifier, error) {
 	defaultSetup := testutils.DefaultTestSetup()
 	oldNodes := defaultSetup.GetAllOldNodesFromTestSetup()
 	newNodes := defaultSetup.GetAllNewNodesFromTestSetup()
@@ -156,7 +249,7 @@ func getCommitmentMessageAndNodesSetup() (DacssCommitmentMessage, *testutils.Pss
 		curves.K256(),
 	)
 	if err != nil {
-		return DacssCommitmentMessage{}, nil, []*testutils.PssTestNode{}, err
+		return DacssCommitmentMessage{}, nil, []*testutils.PssTestNode{}, nil, err
 	}
 
 	concatCommitments := sharing.CompressCommitments(commitments)
@@ -176,5 +269,5 @@ func getCommitmentMessageAndNodesSetup() (DacssCommitmentMessage, *testutils.Pss
 		CurveName:        common.CurveName(curves.K256().Name),
 	}
 
-	return commitmentMsg, receiverNode, senderGroup, nil
+	return commitmentMsg, receiverNode, senderGroup, commitments, nil
 }
