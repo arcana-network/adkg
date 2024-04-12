645d70c2b21f6dd66813412c7167450905c4a10d
Manish Kumar <kumarmanish19963@gmail.com>
2024-04-12 23:23:58 +0530

    more tests for public reconstruction handler

--- dpss/message_handlers/dpss/public_rec_handler_test.go
+++ dpss/message_handlers/dpss/public_rec_handler_test.go
@@ -1,6 +1,7 @@
 package dpss
 
 import (
+	"crypto/rand"
 	"testing"
 	"time"
 
@@ -53,4 +54,89 @@ func getValidPublicRecMsgAndPoints(senderNode *testutils.PssTestNode, defaultSet
 	}
 
 	return &testMsg, points, nil
+}
+
+func TestInvalidShares(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	defaultSetup := testutils.DefaultTestSetup()
+	senderNode := defaultSetup.GetSingleOldNodeFromTestSetup()
+
+	testPrivateMsg, points, err := GetValidPrivateRecMsgAndPoints(senderNode, defaultSetup)
+	assert.Nil(t, err)
+
+	// modify the valid points to trigger an early return
+	curve := testutils.TestCurve()
+
+	// corrupt the shares
+	points[1] = curve.Scalar.Random(rand.Reader)
+	points[2] = curve.Scalar.Random(rand.Reader)
+
+	err = senderNode.State().BatchReconStore.UpdateBatchRecState(
+		getDPSSBatchRecDetails(senderNode).ToBatchRecID(),
+		func(state *common.BatchRecState) {
+			state.ReconstructedUStore = points
+		},
+	)
+	assert.Nil(t, err)
+	testPublicMsg := PublicRecMsg{
+		DPSSBatchRecDetails: testPrivateMsg.DPSSBatchRecDetails,
+		Kind:                PublicRecHandlerType,
+		curveName:           testPrivateMsg.curveName,
+		ReconstructedUShare: testPrivateMsg.UShare,
+	}
+
+	testPublicMsg.Process(senderNode.Details(), senderNode)
+
+	// Wait for all the expected messages to be received
+	time.Sleep(2 * time.Second)
+
+	//No msg is send
+	assert.Equal(t, 0, len(senderNode.Transport().GetSentMessages()))
+
+}
+
+func TestNotEnoughShares(t *testing.T) {
+
+	log.SetLevel(log.DebugLevel)
+	defaultSetup := testutils.DefaultTestSetup()
+	senderNode := defaultSetup.GetSingleOldNodeFromTestSetup()
+	tOld := defaultSetup.OldCommitteeParams.T
+	testPrivateMsg, points, err := GetValidPrivateRecMsgAndPoints(senderNode, defaultSetup)
+	assert.Nil(t, err)
+
+	// creating insufficient points from the valid points to trigger an early return
+	insufficientPoints := make(map[int]curves.Scalar)
+	count := 0
+	for key, value := range points {
+		if count == tOld {
+			break
+		}
+		insufficientPoints[key] = value
+		count += 1
+	}
+
+	// update the state with insufficient points
+	err = senderNode.State().BatchReconStore.UpdateBatchRecState(
+		getDPSSBatchRecDetails(senderNode).ToBatchRecID(),
+		func(state *common.BatchRecState) {
+			state.ReconstructedUStore = insufficientPoints
+		},
+	)
+	assert.Nil(t, err)
+
+	testPublicMsg := PublicRecMsg{
+		DPSSBatchRecDetails: testPrivateMsg.DPSSBatchRecDetails,
+		Kind:                PublicRecHandlerType,
+		curveName:           testPrivateMsg.curveName,
+		ReconstructedUShare: testPrivateMsg.UShare,
+	}
+
+	testPublicMsg.Process(senderNode.Details(), senderNode)
+
+	// Wait for all the expected messages to be received
+	time.Sleep(2 * time.Second)
+
+	//No msg is send
+	assert.Equal(t, 0, len(senderNode.Transport().GetSentMessages()))
+
 }
